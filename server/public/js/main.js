0344dd47b8b684feec03bca4155f5bf5bb6c1d27
André Skv <andre.skvarc@elev.ga.ntig.se>
2020-11-05 10:01:07 +0100

    Added ESlint to the project and fixed all problems in the JS file

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,4 +1,5 @@
-function toggla(event) {
-    let xc90text = document.querySelector(".xc90text")
-    xc90text.classList.toggle("knapp");
+// eslint-disable-next-line no-unused-vars
+function toggla() {
+  const xc90text = document.querySelector('.xc90text');
+  xc90text.classList.toggle('knapp');
 }
